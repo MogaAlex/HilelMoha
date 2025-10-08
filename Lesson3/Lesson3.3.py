98c29b3f616fab4e23d726428681d02915ddcc14
Alex <Mogaalexandr@gmail.co>
2025-10-08 14:29:47 +0300

    second commit

--- Lesson3/Lesson3.3.py
+++ Lesson3/Lesson3.3.py
@@ -1,10 +1,10 @@
-a=[]
+a=[1,2,3]
 size = len(a)
-z = int(size)//2
-x = int(z)
+z = size//2
+x = z
 if len(a)%2 == 0:
-    b = [a[0:x]]+[a[x:int(size)]]
+    b = [a[0:x]]+[a[x:size]]
     print(b)
 else:
-    b = [a[0:x + 1]] + [a[x + 1:int(size)]]
+    b = [a[0:x + 1]] + [a[x + 1:size]]
     print(b)
