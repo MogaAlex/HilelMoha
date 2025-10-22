79df9681faa8087237bc288c44e8925556e333eb
Alex <Mogaalexandr@gmail.co>
2025-10-22 18:27:48 +0300

    second commit

--- Lesson7/Lesson7.3.py
+++ Lesson7/Lesson7.3.py
@@ -1,12 +1,14 @@
 def second_index(text, some_str):
-        if some_str in text:
-            a = text.index(some_str)
-            if (text.find(some_str, a+1)) !=-1:
-                return text.find(some_str, a + 1)
-            else:
-                return None
+    if some_str in text:
+        a = text.find(some_str)
+        if (text.find(some_str, a + 1)) != -1:
+            return text.find(some_str, a + 1)
         else:
             return None
+    else:
+        return None
+
+
 assert second_index("sims", "s") == 3, 'Test1'
 assert second_index("find the river", "e") == 12, 'Test2'
 assert second_index("hi", "h") is None, 'Test3'
