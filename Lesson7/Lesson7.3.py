67cf7cc504fd8f2628c60bc3d6fe80675dc64360
Alex <Mogaalexandr@gmail.co>
2025-10-22 03:26:02 +0300

    first commit

---
+++ Lesson7/Lesson7.3.py
@@ -0,0 +1,14 @@
+def second_index(text, some_str):
+        if some_str in text:
+            a = text.index(some_str)
+            if (text.find(some_str, a+1)) !=-1:
+                return text.find(some_str, a + 1)
+            else:
+                return None
+        else:
+            return None
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
---
+++ Lesson7/Lesson7.4.py
@@ -0,0 +1,17 @@
+def common_elements():
+    set1 = {0}
+    set2 = {0}
+    for i in range(0,100):
+        if i%3==0:
+            set1.add(i)
+        else:
+            continue
+        if i%5==0:
+            set2.add(i)
+        else:
+            continue
+    return set1.intersection(set2)
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+
+
