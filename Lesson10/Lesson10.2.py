d83ffd3b90c3cc7cd4f70f2fdcd349fc3b536c6d
Alex <Mogaalexandr@gmail.co>
2025-10-31 21:24:34 +0200

    first commit

--- Lesson10/Lesson10.2.py
+++ Lesson10/Lesson10.2.py
@@ -21,19 +21,3 @@ assert first_word(".., and so on ...") == "and", 'Test4'
 assert first_word("hi") == "hi", 'Test5'
 assert first_word("Hello.World") == "Hello", 'Test6'
 print('OK')
-
-# text = "hi"
-# #print(string.punctuation.find("'")) # №6
-# new_string_punctuation = string.punctuation[:6]+string.punctuation[7:]
-# for i in new_string_punctuation:
-#     if (i in text)==True:
-#         new_my_string = text.replace(i,' ')
-#         break
-#     else:
-#         new_my_string = text
-#
-# final_string = ""
-# for i in new_my_string:
-#     if (i in new_string_punctuation) == False:
-#         final_string += i
-# print(final_string.split()[0])
