4c25177814dca698c20cb1bb2820effb27b06554
Alex <Mogaalexandr@gmail.co>
2025-11-08 03:34:15 +0200

    Third commit

--- Lesson12/Lesson12.1.py
+++ Lesson12/Lesson12.1.py
@@ -4,16 +4,17 @@ def delete_html_tags(html_file, result_file='cleaned.txt'):
     with codecs.open(html_file, 'w', 'utf-8') as file:
         file.write("hello <fsafas>  <fafa > \n"
                    "fsdgsdgio>>")
-    with (open(html_file, 'r') as file):
-        for line in file:
-            while '<' in line and '>' in line and line.find('<') < line.find('>'):
-                a = line.find('<')
-                b = line.find('>')
-                line = line[0:a] + line[b+1:]
-            print(line)
-        return line
+    with codecs.open(result_file, 'w', 'utf-8') as clean:
+        with (open(html_file, 'r') as file):
+            for line in file:
+                while '<' in line and '>' in line and line.find('<') < line.find('>'):
+                    a = line.find('<')
+                    b = line.find('>')
+                    line = line[0:a] + line[b+1:]
+                clean.write(line)
 
-delete_html_tags('cleaned.txt')
+
+delete_html_tags('basic.txt')
 
 
 
