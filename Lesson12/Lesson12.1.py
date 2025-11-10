ae386f8066abfb9e10a5e368ec4e5b5f09d16b39
MogaAlex <Mogaalexandr@gmail.com>
2025-11-10 08:23:08 -0800

    firth commit

--- Lesson12/Lesson12.1.py
+++ Lesson12/Lesson12.1.py
@@ -1,11 +1,8 @@
 import codecs
 
 def delete_html_tags(html_file, result_file='cleaned.txt'):
-    with codecs.open(html_file, 'w', 'utf-8') as file:
-        file.write("hello <fsafas>  <fafa > \n"
-                   "fsdgsdgio>>")
     with codecs.open(result_file, 'w', 'utf-8') as clean:
-        with (open(html_file, 'r') as file):
+        with open(html_file, 'r') as file:
             for line in file:
                 while '<' in line and '>' in line and line.find('<') < line.find('>'):
                     a = line.find('<')
