c37e52eec9c0973557e3c8d4b4969b57ed54b656
USER <Mogaalexandr@gmail.com>
2025-11-15 01:37:45 +0200

    first commit

---
+++ Lesson14/Lesson14.2/Lesson14.2.py
@@ -0,0 +1,21 @@
+import main
+import zipfile
+import codecs
+
+with zipfile.ZipFile('my_archive.zip', 'w') as myzip:
+    with codecs.open("result_file.txt", 'w', 'utf-8') as result:
+        st1 = main.Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+        st2 = main.Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+        gr = main.Group('PD1')
+        gr.add_student(st1)
+        gr.add_student(st2)
+        result.write(str(gr))
+
+        assert gr.find_student('Jobs') == st1  # 'Steve Jobs'
+        assert gr.find_student('Jobs2') is None
+
+        gr.delete_student('Taylor')
+        result.write(str(gr))  # Only one student
+    myzip.write('main.py')
+    myzip.write('result_file.txt')
+
